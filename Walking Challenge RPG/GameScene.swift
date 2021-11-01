098536ef6d580c434fa24070a2efb8474732c0f9
Arjay Hamilton <hamiltar@oregonstate.edu>
2021-11-01 16:28:31 -0700

    Set up scene

--- Walking Challenge RPG/GameScene.swift
+++ Walking Challenge RPG/GameScene.swift
@@ -1,7 +1,15 @@
 import SpriteKit
- 
+
 class GameScene: SKScene {
     
     let label = SKLabelNode(text: "Walking Challenge RPG")
+    
+    override func didMove(to view: SKView) {
+        
+        label.position = CGPoint(x: view.frame.width / 2, y: view.frame.height / 1.2)
+        
+    addChild(label)
+        
+    }
  
 }
