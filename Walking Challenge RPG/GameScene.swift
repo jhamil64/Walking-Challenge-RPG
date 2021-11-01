6726f1f1e5252f53aaf8a5e228b1418f4765649e
Jaymes Hamilton <hamiljay@oregonstate.edu>
2021-11-01 16:36:16 -0700

    Edited the background and font

--- Walking Challenge RPG/GameScene.swift
+++ Walking Challenge RPG/GameScene.swift
@@ -1,12 +1,21 @@
 import SpriteKit
 
+var sceneColor = UIColor(red: 0.5, green: 0.5, blue: 1.0,
+                         alpha: 1.0)
+
 class GameScene: SKScene {
     
     let label = SKLabelNode(text: "Walking Challenge RPG")
     
     override func didMove(to view: SKView) {
         
+        self.backgroundColor = sceneColor
+        
         label.position = CGPoint(x: view.frame.width / 2, y: view.frame.height / 1.2)
+        
+        label.fontSize = 26
+        label.fontColor = SKColor.black
+        label.fontName = "Helvetica"
         
     addChild(label)
         
