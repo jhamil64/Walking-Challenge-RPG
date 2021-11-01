f1fd4ae5facaa4519c1491b3b6b895fc8cdceb50
Jaymes Hamilton <hamiljay@oregonstate.edu>
2021-11-01 16:53:41 -0700

    Added more labels for later

--- Walking Challenge RPG/GameScene.swift
+++ Walking Challenge RPG/GameScene.swift
@@ -5,19 +5,32 @@ var sceneColor = UIColor(red: 0.5, green: 0.5, blue: 1.0,
 
 class GameScene: SKScene {
     
-    let label = SKLabelNode(text: "Walking Challenge RPG")
+    let title = SKLabelNode(text: "Walking Challenge RPG")
+    let stepText = SKLabelNode(text: "Steps Walked")
+    let moneyText = SKLabelNode(text: "Total Gold")
     
     override func didMove(to view: SKView) {
         
         self.backgroundColor = sceneColor
         
-        label.position = CGPoint(x: view.frame.width / 2, y: view.frame.height / 1.2)
+        title.position = CGPoint(x: view.frame.width / 2, y: view.frame.height / 1.2)
+        title.fontSize = 26
+        title.fontColor = SKColor.black
+        title.fontName = "Helvetica"
         
-        label.fontSize = 26
-        label.fontColor = SKColor.black
-        label.fontName = "Helvetica"
+        stepText.position = CGPoint(x: view.frame.width / 2, y: view.frame.height / 1.6)
+        stepText.fontSize = 18
+        stepText.fontColor = SKColor.black
+        stepText.fontName = "Helvetica"
         
-    addChild(label)
+        moneyText.position = CGPoint(x: view.frame.width / 2, y: view.frame.height / 2.0)
+        moneyText.fontSize = 18
+        moneyText.fontColor = SKColor.black
+        moneyText.fontName = "Helvetica"
+        
+    addChild(title)
+    addChild(stepText)
+    addChild(moneyText)
         
     }
  
