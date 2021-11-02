dbe51722cefa3ca890768dfed20109a0554fdaac
Jaymes Hamilton <hamiljay@oregonstate.edu>
2021-11-01 20:08:17 -0700

    Made the screen look close to the mockup. It should be good for now; actual functions will come later. I also need to add the options icon later too.

--- Walking Challenge RPG/GameScene.swift
+++ Walking Challenge RPG/GameScene.swift
@@ -8,6 +8,8 @@ class GameScene: SKScene {
     let title = SKLabelNode(text: "Walking Challenge RPG")
     let stepText = SKLabelNode(text: "Steps Walked")
     let moneyText = SKLabelNode(text: "Total Gold")
+    let buttonText = SKLabelNode(text: "To Castle Area")
+    let buttonTexture = SKSpriteNode(color: .blue, size: CGSize(width: 100, height: 100))
     
     override func didMove(to view: SKView) {
         
@@ -18,19 +20,29 @@ class GameScene: SKScene {
         title.fontColor = SKColor.black
         title.fontName = "Helvetica"
         
-        stepText.position = CGPoint(x: view.frame.width / 2, y: view.frame.height / 1.6)
-        stepText.fontSize = 18
+        stepText.position = CGPoint(x: view.frame.width / 2, y: view.frame.height / 1.5)
+        stepText.fontSize = 21
         stepText.fontColor = SKColor.black
         stepText.fontName = "Helvetica"
         
         moneyText.position = CGPoint(x: view.frame.width / 2, y: view.frame.height / 2.0)
-        moneyText.fontSize = 18
+        moneyText.fontSize = 21
         moneyText.fontColor = SKColor.black
         moneyText.fontName = "Helvetica"
+        
+        buttonTexture.position = CGPoint(x: view.frame.width / 2, y: view.frame.height / 4)
+        buttonTexture.size = CGSize(width: view.frame.width / 1.5, height: view.frame.height / 8)
+        
+        buttonText.position = CGPoint(x: view.frame.width / 2, y: view.frame.height / 4.1)
+        buttonText.fontSize = 23
+        buttonText.fontColor = SKColor.white
+        buttonText.fontName = "Courier New Bold"
         
     addChild(title)
     addChild(stepText)
     addChild(moneyText)
+    addChild(buttonTexture)
+    addChild(buttonText)
         
     }
  
