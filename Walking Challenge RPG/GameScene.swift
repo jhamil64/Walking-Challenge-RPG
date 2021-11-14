bd95598923f23e1c992612a9dc816244272c859f
Arjay Hamilton <hamiltar@oregonstate.edu>
2021-11-14 15:29:39 -0800

    Almost done implementing pedometer code.

--- Walking Challenge RPG/GameScene.swift
+++ Walking Challenge RPG/GameScene.swift
@@ -1,7 +1,17 @@
 import SpriteKit
+import CoreMotion
+import UIKit
 
 var sceneColor = UIColor(red: 0.6, green: 0.6, blue: 1.0,
                          alpha: 1.0)
+var pedometer = CMPedometer()
+var distance:Double! = nil
+var averagePace:Double! = nil
+var pace:Double! = nil
+var numberOfSteps:Int! = nil
+var timer = Timer()
+var timerInterval = 1.0
+var timeElapsed:TimeInterval = 1.0
 
 class GameScene: SKScene {
     
@@ -11,10 +21,51 @@ class GameScene: SKScene {
     let buttonText = SKLabelNode(text: "To Castle Area")
     let challengeText = SKLabelNode(text: "Active Challenges")
     let buttonTexture = SKSpriteNode(color: .blue, size: CGSize(width: 100, height: 100))
+    let statusTitle = SKLabelNode(text: "Text")
+    let stepsLabel = SKLabelNode(text: "steps")
+    let avgPaceLabel = SKLabelNode(text: "avgPace")
+    let paceLabel = SKLabelNode(text: "pace")
+    let distanceLabel = SKLabelNode(text: "distance")
+    
+
+     
+    func timeIntervalFormat(interval:TimeInterval)-> String{
+            var seconds = Int(interval + 0.5) //round up seconds
+            let hours = seconds / 3600
+            let minutes = (seconds / 60) % 60
+            seconds = seconds % 60
+            return String(format:"%02i:%02i:%02i",hours,minutes,seconds)
+        }
+    
+    
+    func displayData()
+    {
+//        stepsLabel.text = String(format: "%i", numberOfSteps)
+        pedometer.startUpdates(from: Date(), withHandler: { (pedometerData, error) in
+                        if let pedData = pedometerData{
+                            numberOfSteps = Int(truncating: pedData.numberOfSteps)
+                            //self.stepsLabel.text = "Steps:\(pedData.numberOfSteps)"
+                            
+                        } else {
+                            numberOfSteps = nil
+                        }
+                    })
+    }
+    
+    func displayPedometerData()
+    {
+            timeElapsed += 1.0
+            statusTitle.text = "On: " + timeIntervalFormat(interval: timeElapsed)
+            //Number of steps
+             
+                stepsLabel.text = String(format:"Steps: %i",numberOfSteps)
+    }
     
     override func didMove(to view: SKView) {
         
         self.backgroundColor = sceneColor
+        
+        stepsLabel.position = CGPoint(x: view.frame.width / 2, y: view.frame.height / 1.7)
         
         title.position = CGPoint(x: view.frame.width / 2, y: view.frame.height / 1.2)
         title.fontSize = 26
@@ -50,6 +101,9 @@ class GameScene: SKScene {
     addChild(challengeText)
     addChild(buttonTexture)
     addChild(buttonText)
+    addChild(stepsLabel)
+        
+        displayData()
         
     }
  
