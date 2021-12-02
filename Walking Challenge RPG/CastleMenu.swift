7c3f5f5b180de5ebb5ea3b3286bb2b4d6addd482
Jaymes Hamilton <hamiljay@oregonstate.edu>
2021-12-01 21:50:44 -0800

    Initialized the Castle Menu, and changed the game to support landscape orientation. Will make more edits relating to this later. We realized that changing orientation of the game unnecessarily complicates things.

--- Walking Challenge RPG/CastleMenu.swift
+++ Walking Challenge RPG/CastleMenu.swift
@@ -1,14 +1,18 @@
-//
-//  CastleMenu.swift
-//  Walking Challenge RPG
-//
-//  Created by Chibi on 2021/12/01.
-//
+
 
 import Foundation
 import SpriteKit
 
 class CastleMenu: SKScene {
     
+    var sceneColor = UIColor(red: 0.6, green: 1.0, blue: 0.6,
+                             alpha: 1.0)
+    let title = SKLabelNode(text: "Walking Challenge RPG")
+    override func didMove(to view: SKView) {
+        self.backgroundColor = sceneColor
+        title.position = CGPoint(x: view.frame.width / 2, y: view.frame.height / 1.2)
+        
+    addChild(title)
+    }
     
 }
--- Walking Challenge RPG/GameViewController.swift
+++ Walking Challenge RPG/GameViewController.swift
@@ -1,4 +1,5 @@
 import SpriteKit
+import UIKit
  
 class GameViewController: UIViewController {
     
