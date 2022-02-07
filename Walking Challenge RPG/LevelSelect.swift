79247e036527c0383d9ea6d1dabe6dfc99f389b0
Jaymes Hamilton <hamiljay@oregonstate.edu>
2022-02-06 19:28:46 -0800

    All buttons necessary for traversal enabled. I could change the textures of the buttons, but finding images is not really necessary right now. I have other things I have to do.

--- Walking Challenge RPG/LevelSelect.swift
+++ Walking Challenge RPG/LevelSelect.swift
@@ -2,16 +2,40 @@
 import Foundation
 import SpriteKit
 
-class LevelSelect: SKScene {
+class LevelSelect: SKScene, ButtonDelegate, BackButtonDelegate {
+    
+    private var button = Button()
     
     var sceneColor = UIColor(red: 0.4, green: 1.0, blue: 0.4,
                              alpha: 1.0)
-    let title = SKLabelNode(text: "Walking Challenge RPG")
+    let title = SKLabelNode(text: "Greenhorn Grassland")
+    
     override func didMove(to view: SKView) {
         self.backgroundColor = sceneColor
         title.position = CGPoint(x: view.frame.width / 2, y: view.frame.height / 1.2)
         
+        let button3 = BackButton(texture: nil, color: .black, size: CGSize(width: view.frame.width / 10, height: view.frame.height / 10))
+        button3.position = CGPoint(x: view.frame.width / 100, y: view.frame.height / 1.1)
+        button3.delegate = self
+                addChild(button3)
+        
     addChild(title)
+        
+        
+    }
+    
+    func buttonClicked(sender: Button) {
+        let transition:SKTransition = SKTransition.fade(withDuration: 1)
+        let scene: SKScene = LevelSelect(size: self.size)
+        self.view?.presentScene(scene, transition: transition)
+        
+    }
+    
+    func backButtonClicked(sender: BackButton) {
+        let transition:SKTransition = SKTransition.fade(withDuration: 1)
+        let scene: SKScene = WorldMap(size: self.size)
+        self.view?.presentScene(scene, transition: transition)
+        
     }
     
 }
--- Walking Challenge RPG/WorldMap.swift
+++ Walking Challenge RPG/WorldMap.swift
@@ -2,7 +2,9 @@
 import Foundation
 import SpriteKit
 
-class WorldMap: SKScene, ButtonDelegate {
+
+
+class WorldMap: SKScene, ButtonDelegate, BackButtonDelegate {
     
     private var button = Button()
     
@@ -11,6 +13,8 @@ class WorldMap: SKScene, ButtonDelegate {
     let title = SKLabelNode(text: "Walking Challenge RPG")
     let buttonText = SKLabelNode(text: "To World Map")
     let buttonTexture = SKSpriteNode(color: .blue, size: CGSize(width: 100, height: 100))
+    let buttonTexture2 = SKSpriteNode(color: .black, size: CGSize(width: 10, height: 10))
+    
     
     override func didMove(to view: SKView) {
         if let button = self.childNode(withName: "button") as? Button {
@@ -18,14 +22,22 @@ class WorldMap: SKScene, ButtonDelegate {
                     button.delegate = self
                 }
 
-                let button2 = Button(texture: nil, color: .blue, size: CGSize(width: view.frame.width / 1.5, height: view.frame.height / 8))
+                let button2 = Button(texture: nil, color: .blue, size: CGSize(width: view.frame.width / 10, height: view.frame.height / 10))
+        let button3 = BackButton(texture: nil, color: .black, size: CGSize(width: view.frame.width / 10, height: view.frame.height / 10))
                 button2.name = "button2"
-                button2.position = CGPoint(x: view.frame.width / 2, y: view.frame.height / 7.2)
+        button2.position = CGPoint(x: view.frame.width / 6, y: view.frame.height / 1.5)
                 button2.delegate = self
+        button3.position = CGPoint(x: view.frame.width / 100, y: view.frame.height / 1.1)
+        button3.delegate = self
                 addChild(button2)
+                addChild(button3)
         
-        buttonTexture.position = CGPoint(x: view.frame.width / 2, y: view.frame.height / 7.2)
-        buttonTexture.size = CGSize(width: view.frame.width / 1.5, height: view.frame.height / 8)
+        
+//
+//        buttonTexture.position = CGPoint(x: view.frame.width / 2, y: view.frame.height / 7.2)
+//        buttonTexture.size = CGSize(width: view.frame.width / 1.5, height: view.frame.height / 8)
+//        buttonTexture2.position = CGPoint(x: view.frame.width / 10, y: view.frame.height / 1)
+//        buttonTexture2.size = CGSize(width: view.frame.width / 10, height: view.frame.height / 3)
         
         buttonText.position = CGPoint(x: view.frame.width / 2, y: view.frame.height / 7.4)
         buttonText.fontSize = 23
@@ -40,6 +52,13 @@ class WorldMap: SKScene, ButtonDelegate {
     func buttonClicked(sender: Button) {
         let transition:SKTransition = SKTransition.fade(withDuration: 1)
         let scene: SKScene = LevelSelect(size: self.size)
+        self.view?.presentScene(scene, transition: transition)
+        
+    }
+    
+    func backButtonClicked(sender: BackButton) {
+        let transition:SKTransition = SKTransition.fade(withDuration: 1)
+        let scene: SKScene = CastleMenu(size: self.size)
         self.view?.presentScene(scene, transition: transition)
         
     }
