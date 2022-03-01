01a05e5738f004db9aecd3d0857c8d4e498847c7
Jaymes Hamilton <hamiljay@oregonstate.edu>
2022-02-28 16:22:12 -0800

    Created "level 1". It is still under development right now.

--- Walking Challenge RPG/Level1.swift
+++ Walking Challenge RPG/Level1.swift
@@ -2,37 +2,31 @@
 import Foundation
 import SpriteKit
 
-class LevelSelect: SKScene, ButtonDelegate, BackButtonDelegate {
+class Level1: SKScene, ButtonDelegate, BackButtonDelegate {
     
     private var button = Button()
     
-    var sceneColor = UIColor(red: 0.4, green: 1.0, blue: 0.4,
+    var sceneColor = UIColor(red: 0.6, green: 1.0, blue: 0.6,
                              alpha: 1.0)
-    let title = SKLabelNode(text: "Greenhorn Grassland")
+//    let title = SKLabelNode(text: "Greenhorn Grassland")
     
     override func didMove(to view: SKView) {
         self.backgroundColor = sceneColor
-        title.position = CGPoint(x: view.frame.width / 2, y: view.frame.height / 1.2)
-        
-        let button2 = Button(texture: nil, color: .black, size: CGSize(width: view.frame.width / 10, height: view.frame.height / 10))
-        button2.name = "stage1"
-        button2.position = CGPoint(x: view.frame.width / 6, y: view.frame.height / 1.5)
-        button2.delegate = self
+//        title.position = CGPoint(x: view.frame.width / 2, y: view.frame.height / 1.2)
         
         let button3 = BackButton(texture: nil, color: .black, size: CGSize(width: view.frame.width / 10, height: view.frame.height / 10))
         button3.position = CGPoint(x: view.frame.width / 100, y: view.frame.height / 1.1)
         button3.delegate = self
-                addChild(button2)
                 addChild(button3)
         
-    addChild(title)
+//    addChild(title)
         
         
     }
     
     func buttonClicked(sender: Button) {
         let transition:SKTransition = SKTransition.fade(withDuration: 1)
-        let scene: SKScene = Level1(size: self.size)
+        let scene: SKScene = LevelSelect(size: self.size)
         self.view?.presentScene(scene, transition: transition)
         
     }
